fb290dd5203900ebcba7c948f2d13203f7e6abef
JoeChen07 <545841225@qq.com>
2024-02-22 15:21:36 +0800

    完成gatsby 教程3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `What would you like to call your site?✔ · My First Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
