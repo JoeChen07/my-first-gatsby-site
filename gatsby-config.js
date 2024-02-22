19e39c166ef0bc707439a039dffd6907ca251120
JoeChen07 <545841225@qq.com>
2024-02-22 20:05:46 +0800

    完成Gatsby教程7

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,22 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-module.exports = {
-  siteMetadata: {
-    title: `My First Gatsby Site`,
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
-      }
+ module.exports = {
+    siteMetadata: {
+      title: `My First Gatsby Site`,
     },
-    "gatsby-plugin-mdx",
-  ],
-};
+    plugins: [
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
+      "gatsby-plugin-mdx",
+      "gatsby-transformer-sharp",
+    ],
+  };
+  
